36956db43e070440d676ae02fc947d5f4b5209e1
Antonio Marino <antonio.marino@cern.ch>
2015-04-26 17:46:32 +0200

    Add Parse

--- jamclub/app/src/main/java/com/magnaideas/jamclub/Activities/EarnPaymentActivity.java
+++ jamclub/app/src/main/java/com/magnaideas/jamclub/Activities/EarnPaymentActivity.java
@@ -1,6 +1,7 @@
 package com.magnaideas.jamclub.Activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +10,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.magnaideas.jamclub.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * Created by edoardomoreni on 11/04/2015.
@@ -25,7 +38,49 @@ public class EarnPaymentActivity extends ActionBarActivity {
         setContentView(R.layout.activity_earnpayment);
 
         Intent intent = getIntent();
-        Log.d(TAG, intent.getStringExtra("user_info"));
+        Log.d(TAG, intent.getStringExtra("access_token"));
+        final String access_token = intent.getStringExtra("access_token");
+
+        AsyncTask<String,String,String> task = new AsyncTask<String, String, String>() {
+            @Override
+            protected String doInBackground(String... params) {
+
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse response;
+                String responseString = null;
+                try {
+                    response = httpclient.execute(new HttpGet("https://jamclub.herokuapp.com/me?access_token="+access_token));
+                    StatusLine statusLine = response.getStatusLine();
+                    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                        ByteArrayOutputStream out = new ByteArrayOutputStream();
+                        response.getEntity().writeTo(out);
+                        responseString = out.toString();
+                        out.close();
+                    } else{
+                        //Closes the connection.
+                        response.getEntity().getContent().close();
+                        throw new IOException(statusLine.getReasonPhrase());
+                    }
+                } catch (ClientProtocolException e) {
+                    //TODO Handle problems..
+                    Log.e(TAG, e.getMessage());
+                } catch (IOException e) {
+                    //TODO Handle problems..
+                    Log.e(TAG, e.getMessage());
+                }
+                return responseString;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                Log.d(TAG, result);
+            }
+
+        };
+
+        task.execute();
+
         /*
         mToolbar = (Toolbar)findViewById(R.id.toolbar);
 
