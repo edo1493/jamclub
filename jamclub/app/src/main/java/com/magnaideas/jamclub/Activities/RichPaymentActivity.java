495e3a7bbc2673d740971870c0fa50bb9d0a484e
Antonio Marino <antonio.marino@cern.ch>
2015-05-05 00:10:54 +0200

    Payment with paypal

--- jamclub/app/src/main/java/com/magnaideas/jamclub/Activities/RichPaymentActivity.java
+++ jamclub/app/src/main/java/com/magnaideas/jamclub/Activities/RichPaymentActivity.java
@@ -1,14 +1,29 @@
 package com.magnaideas.jamclub.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.magnaideas.jamclub.R;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 
 /**
  * Created by edoardomoreni on 11/04/2015.
@@ -19,6 +34,33 @@ public class RichPaymentActivity extends ActionBarActivity {
     private TextView mLongitude;
     private double latitude;
     private double longitude;
+
+    /**
+     * - Set to PayPalConfiguration.ENVIRONMENT_PRODUCTION to move real money.
+     *
+     * - Set to PayPalConfiguration.ENVIRONMENT_SANDBOX to use your test credentials
+     * from https://developer.paypal.com
+     *
+     * - Set to PayPalConfiguration.ENVIRONMENT_NO_NETWORK to kick the tires
+     * without communicating to PayPal's servers.
+     */
+    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
+
+    // note that these credentials will differ between live & sandbox environments.
+    private static final String CONFIG_CLIENT_ID = "AYimypdxdb4VUaOWk5RRy0hPMcZu1jAvkC1U5TTn79oJsWkl7U92AkXhPsGoSGa5uVR5xsUeu2Jdrq_F";
+
+    private static final int REQUEST_CODE_PAYMENT = 1;
+    private static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
+    private static final int REQUEST_CODE_PROFILE_SHARING = 3;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(CONFIG_ENVIRONMENT)
+            .clientId(CONFIG_CLIENT_ID)
+                    // The following are only used in PayPalFuturePaymentActivity.
+            .merchantName("Example Merchant")
+            .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
+            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +81,73 @@ public class RichPaymentActivity extends ActionBarActivity {
         mLongitude = (TextView)findViewById(R.id.longitude);
         mLongitude.setText(" " + longitude);
 
-
     }
+
+    public void pay(View view) {
+
+        EditText budgetEditText = (EditText) findViewById(R.id.budget);
+        String budgetString = budgetEditText.getText().toString();
+        BigDecimal budget;
+        try {
+            budget = new BigDecimal(budgetString);
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Not a valid amount", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        /*
+         * PAYMENT_INTENT_SALE will cause the payment to complete immediately.
+         * Change PAYMENT_INTENT_SALE to
+         *   - PAYMENT_INTENT_AUTHORIZE to only authorize payment and capture funds later.
+         *   - PAYMENT_INTENT_ORDER to create a payment for authorization and capture
+         *     later via calls from your server.
+         *
+         * Also, to include additional payment details and an item list, see getStuffToBuy() below.
+         */
+        PayPalPayment payPalPayment = new PayPalPayment(
+                budget,
+                "USD",
+                "JamClub",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent(this, PaymentActivity.class);
+
+        // send the same configuration for restart resiliency
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payPalPayment);
+
+        startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+            if (confirm != null) {
+                try {
+                    Log.i("paymentExample", confirm.toJSONObject().toString(4));
+
+                    // TODO: send 'confirm' to your server for verification.
+                    // see https://developer.paypal.com/webapps/developer/docs/integration/mobile/verify-mobile-payment/
+                    // for more details.
+
+                    Intent intent = new Intent(this, AttackStatusActivity.class);
+                    startActivity(intent);
+
+                } catch (JSONException e) {
+                    Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
+                }
+            }
+        }
+        else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.i("paymentExample", "The user canceled.");
+        }
+        else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+            Log.i("paymentExample", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
