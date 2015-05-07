86637843c7b8d5a380ed7ca7d19c15c5beb1e48c
Edoardo Moreni <edo1493@yahoo.it>
2015-05-07 20:18:36 +0100

    roboto, assets directory and change to call to arms

---
+++ jamclub/app/src/main/java/com/magnaideas/jamclub/Utils/CustomTextView.java
@@ -0,0 +1,35 @@
+package com.magnaideas.jamclub.Utils;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by edoardomoreni on 07/05/15.
+ */
+public class CustomTextView extends TextView {
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "roboto.ttf");
+            setTypeface(tf);
+        }
+    }
+
+}
